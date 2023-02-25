ed97433556ea2f6e047fd3e524fee8829e7d39ca
김주환 <112999677+kimjuhwan7@users.noreply.github.com>
2023-02-25 23:17:38 +0900

    Initial commit

---
+++ src/main/java/com/juLog/JuLogApplication.java
@@ -0,0 +1,13 @@
+package com.juLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juLog/JuLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
